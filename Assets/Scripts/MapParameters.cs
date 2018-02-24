a9c7bb444b51d62577cf03a9c91bdbe3f5e902d7
Aleksis Andzans <0andza31@solent.ac.uk>
2018-02-24 18:23:01 +0000

    Expanded TileType and MapParameters classes Added Range struct

--- Assets/Scripts/MapParameters.cs
+++ Assets/Scripts/MapParameters.cs
@@ -6,20 +6,181 @@ namespace WorldMapGen
     [System.Serializable]
     public class MapParameters
     {
+        // If true, use a custom random seed
+        [SerializeField]
+        protected bool customSeed;
+        public bool CustomSeed
+        {
+            get { return customSeed; }
+        }
+
+        [SerializeField]
+        protected int seed;
+        public int Seed
+        {
+            get { return seed; }
+        }
+
         // Number of tiles in each axis
         [SerializeField]
         protected int width;
         public int Width
         {
             get { return width; }
-            protected set { width = value; }
         }
+
         [SerializeField]
         protected int height;
         public int Height
         {
             get { return height; }
-            protected set { height = value; }
+        }
+
+        [SerializeField]
+        protected Vector2 tileScale;
+        public Vector2 TileScale
+        {
+            get { return tileScale; }
+        }
+
+        [SerializeField]
+        protected float oceanPercentage;
+        public float OceanPercentage
+        {
+            get { return oceanPercentage; }
+        }
+
+        [SerializeField]
+        protected bool rotateWest;
+        public bool RotateWest
+        {
+            get { return rotateWest; }
+        }
+
+        [SerializeField]
+        protected bool wrapX;
+        public bool WrapX
+        {
+            get { return wrapX; }
+        }
+
+        [SerializeField]
+        protected bool wrapY;
+        public bool WrapY
+        {
+            get { return wrapY; }
+        }
+
+        [SerializeField]
+        protected float noiseScale;
+        public float NoiseScale
+        {
+            get { return noiseScale; }
+        }
+
+        [SerializeField]
+        protected TileType[] tileTypes;
+        public TileType[] TileTypes
+        {
+            get { return tileTypes; }
+        }
+
+        [SerializeField]
+        protected float highPressureLatitude;
+        public float HighPressureLatitude
+        {
+            get { return highPressureLatitude; }
+        }
+
+        [SerializeField]
+        protected float lowPressureLatitude;
+        public float LowPressureLatitude
+        {
+            get { return lowPressureLatitude; }
+        }
+
+        [SerializeField]
+        protected float equatorTemperature;
+        public float EquatorTemperature
+        {
+            get { return equatorTemperature; }
+        }
+
+        [SerializeField]
+        protected float poleTemperature;
+        public float PoleTemperature
+        {
+            get { return poleTemperature; }
+        }
+
+        [SerializeField]
+        protected float temperatureLapseRate;
+        public float TemperatureLapseRate
+        {
+            get { return temperatureLapseRate; }
+        }
+
+        [SerializeField]
+        protected float equatorRainfall;
+        public float EquatorRainfall
+        {
+            get { return equatorRainfall; }
+        }
+
+        [SerializeField]
+        protected float equatorRainfallEvenness;
+        public float EquatorRainfallEvenness
+        {
+            get { return equatorRainfallEvenness; }
+        }
+
+        [SerializeField]
+        protected float midLatitudeRainfall;
+        public float MidLatitudeRainfall
+        {
+            get { return midLatitudeRainfall; }
+        }
+
+        [SerializeField]
+        protected float midLatitudeRainfallEvenness;
+        public float MidLatitudeRainfallEvenness
+        {
+            get { return midLatitudeRainfallEvenness; }
+        }
+
+        [SerializeField]
+        protected float rainfallOceanEFoldingDistance;
+        public float RainfallOceanEFoldingDistance
+        {
+            get { return rainfallOceanEFoldingDistance; }
+        }
+
+        [SerializeField]
+        protected float condensationRateMultiplier;
+        public float CondensationRateMultiplier
+        {
+            get { return condensationRateMultiplier; }
+        }
+
+        [SerializeField]
+        protected float saturationPressureExponentMultiplier;
+        public float SaturationPressureExponentMultiplier
+        {
+            get { return saturationPressureExponentMultiplier; }
+        }
+
+        [SerializeField]
+        protected float saturationPressureExponentDivisor;
+        public float SaturationPressureExponentDivisor
+        {
+            get { return saturationPressureExponentDivisor; }
+        }
+
+        [SerializeField]
+        protected float moistureScaleHeightDivisor;
+        public float MoistureScaleHeightDivisor
+        {
+            get { return moistureScaleHeightDivisor; }
         }
     }
 }
--- Assets/Scripts/TileType.cs
+++ Assets/Scripts/TileType.cs
@@ -1,7 +1,124 @@
-﻿namespace WorldMapGen
+﻿using UnityEngine;
+
+namespace WorldMapGen
 {
+    // Minimum and maximum float value
+    [System.Serializable]
+    public struct Range
+    {
+        // The minimum value
+        [SerializeField]
+        private float min;
+        public float Min
+        {
+            get { return min; }
+        }
+
+        // The maximum value
+        [SerializeField]
+        private float max;
+        public float Max
+        {
+            get { return max; }
+        }
+
+        // Return whether the given value is within this range
+        public bool IsInRange(float value)
+        {
+            return value >= min && value <= max;
+        }
+    }
+
+    // A type of tile and parameters that specify where it can be generated
     [System.Serializable]
     public class TileType
     {
+        // The type's name
+        [SerializeField]
+        protected string name;
+        public string Name
+        {
+            get { return name; }
+        }
+
+        // Ranges of elevations (m above sea level) where the type may appear
+        [SerializeField]
+        protected Range[] elevation;
+        public Range[] Elevation
+        {
+            get { return elevation; }
+        }
+
+        // Ranges of temperatures (°C) where the type may appear
+        [SerializeField]
+        protected Range[] temperature;
+        public Range[] Temperature
+        {
+            get { return temperature; }
+        }
+
+        // Ranges of precipitation amounts (mm/year) where the type may appear
+        [SerializeField]
+        protected Range[] precipitation;
+        public Range[] Precipitation
+        {
+            get { return precipitation; }
+        }
+
+        // The sprite for this type
+        [SerializeField]
+        protected Sprite sprite;
+
+        // If this type's elevation ranges include both positive and negative
+        // values, return false
+        public bool ElevationIsValid()
+        {
+            bool positive = false;  // Is the highest max value positive?
+            bool negative = false;  // Is the lowest min value negative?
+
+            foreach (Range range in elevation)
+            {
+                if (range.Min < 0.0f)
+                {
+                    negative = true;
+                }
+
+                if (range.Max > 0.0f)
+                {
+                    positive = true;
+                }
+
+                if (positive && negative)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Return whether the given value is within any of the ranges in the
+        // given array
+        protected bool ValueInAnyRange(Range[] ranges, float value)
+        {
+            foreach (Range range in ranges)
+            {
+                if (range.IsInRange(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Return whether the given values for elevation, temperature and
+        // precipitation are within this type's ranges
+        public bool ValuesInRanges(
+            float elevation, float temperature, float precipitation)
+        {
+            return 
+                ValueInAnyRange(this.elevation, elevation) &&
+                ValueInAnyRange(this.temperature, temperature) &&
+                ValueInAnyRange(this.precipitation, precipitation);
+        }
     }
 }
