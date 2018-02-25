4858a734bf1c152d35a59fa62281ea4a66b05b9d
Aleksis Andzans <0andza31@solent.ac.uk>
2018-02-25 13:22:06 +0000

    Expanded Tile class

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,8 +1,21 @@
 ﻿namespace WorldMapGen
 {
+    // An individual tile in the generated map
     public class Tile
     {
+        // The type of the tile
+        public TileType Type { get; set; }
+
         // Metres above sea level
-        public float Height { get; set; }
+        public float Elevation { get; set; }
+
+        // Annual average temperature in °C
+        public float Temperature { get; set; }
+
+        // Annual average precipitation in mm
+        public float Precipitation { get; set; }
+
+        // Distance to nearest ocean tile in km
+        public float OceanDistance { get; set; }
     }
 }
