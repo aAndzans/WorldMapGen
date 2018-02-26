72bb232ce12119e5a576e7b08f76f239e52da501
Aleksis Andzans <0andza31@solent.ac.uk>
2018-02-26 16:41:46 +0000

    Wrote heightmap generation code

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,9 @@ namespace WorldMapGen
     // Class that procedurally generates a map
     public class MapGenerator : MonoBehaviour
     {
+        // Maximum X and Y offset for heightmap noise
+        protected const float noiseMaxOffset = 1000.0f;
+
         // User-specified parameters for map generation
         [SerializeField]
         protected MapParameters parameters;
@@ -18,58 +21,93 @@ namespace WorldMapGen
         // Map currently being generated
         protected Tilemap currentMap;
 
-        protected virtual void GenerateHeightmap()
-        {
-            float perlinScale = Mathf.Max(parameters.Width, parameters.Height);
-            for (int i = 0; i < parameters.Height; i++)
-            {
-                for (int j = 0; j < parameters.Width; j++)
-                {
-                    Tile newTile = new Tile();
-                    currentMap.SetTile(new Vector3Int(j, i, 0), newTile);
-                    newTile.Elevation = Mathf.PerlinNoise(i / perlinScale, j / perlinScale);
-                    Debug.Log(newTile.Elevation);
-                }
-            }
-        }
+        // Number of tiles corresponding to 1 unit in the noise function
+        protected float noiseScale;
+        // Noise function offset
+        protected Vector2 noiseOffset;
+
+        // Highest possible elevation
+        protected float maxElevation;
 
         // Procedurally generate a map, storing it in the given tilemap
         public virtual void GenerateMap(Tilemap map)
         {
             currentMap = map;
-            GenerateHeightmap();
+
+            GenerateInitialValues();
+            AdjustRainfallForOceanDistance();
+            AdjustOrographicRainfall();
+
+            currentMap = null;
         }
 
         // Generate all tile properties that do not depend on other tiles
         protected virtual void GenerateInitialValues()
         {
+            InitElevationGenerator();
 
+            for (int i = 0; i < parameters.Height; i++)
+            {
+                for (int j = 0; j < parameters.Width; j++)
+                {
+                    Tile newTile = new Tile();
+                    // Generate elevation
+                    newTile.Elevation = ElevationAtCoords(j, i);
+                    // Place the tile in the map
+                    currentMap.SetTile(new Vector3Int(j, i, 0), newTile);
+                    Debug.Log(newTile.Elevation);
+                }
+            }
+        }
+
+        // Initialise the variables used for generating elevation
+        protected virtual void InitElevationGenerator()
+        {
+            noiseScale = parameters.NoiseScale *
+                         Mathf.Max(parameters.Width, parameters.Height);
+            noiseOffset = new Vector2(Random.Range(0.0f, noiseMaxOffset),
+                                      Random.Range(0.0f, noiseMaxOffset));
+
+            // Maximum elevation is the highest elevation among all tile types
+            maxElevation = -Mathf.Infinity;
+            foreach (TileType type in parameters.TileTypes)
+            {
+                foreach (Range range in type.Elevation)
+                {
+                    if (range.Max > maxElevation)
+                    {
+                        maxElevation = range.Max;
+                    }
+                }
+            }
         }
 
         // Use noise to generate an elevation corresponding to the given
         // coordinates
         protected virtual float ElevationAtCoords(int x, int y)
         {
-
+            return Mathf.PerlinNoise(
+                x / noiseScale + noiseOffset.x,
+                y / noiseScale + noiseOffset.y) * maxElevation;
         }
 
         // Calculate temperature based on latitude for the given Y coordinate
         protected virtual float TemperatureAtY(int y)
         {
-
+            return 0.0f;
         }
 
         // Adjust the given temperature based on the given elevation
         protected virtual float TemperatureAtElevation(
             float temperature, float elevation)
         {
-
+            return 0.0f;
         }
 
         // Calculate precipitation based on latitude for the given Y coordinate
         protected virtual float RainfallAtY(int y)
         {
-
+            return 0.0f;
         }
 
         // Reduce the precipitation at all land tiles based on their distance
